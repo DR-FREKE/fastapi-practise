8b42c05e0062477ddedfb4f842de3264019c4d07
solomonndifreke <solomonndi96@gmail.com>
2024-01-09 14:13:58 +0100

    auth login commit

--- TodoApp/router/auth.py
+++ TodoApp/router/auth.py
@@ -1,14 +1,11 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
+from typing import Annotated
 from schema import UserSchema
 from middlewares.db_middleware import db_dependency
 from model import model
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
-
-
-@router.get("/auth/login")
-async def get_user():
-    return {"user": "authenticated"}
 
 
 @router.post("/auth/signup", status_code=status.HTTP_201_CREATED)
@@ -16,3 +13,8 @@ async def create_user(data: UserSchema, db: db_dependency):
     user_data = model.Users(**data.dict())
     db.add(user_data)
     db.commit()
+
+
+@router.post("/auth/token", status_code=status.HTTP_200_OK)
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    return form_data.username
--- TodoApp/services/password.py
+++ TodoApp/services/password.py
@@ -6,10 +6,11 @@ from passlib.context import CryptContext
 class PasswordHash:
     __bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-    @staticmethod
-    def toHash(password: str) -> str:
-        hashed_password = PasswordHash.__bcrypt_context.hash(password)
+    @classmethod
+    def toHash(cls, password: str) -> str:
+        hashed_password = cls.__bcrypt_context.hash(password)
         return hashed_password
 
-    async def comparePassword(self):
+    @staticmethod
+    def comparePassword():
         pass
